2e0dd97de35ed7518bb452a8ea1ecfbedc6bbb84
Mohsen Mesgar <mesgar@ukp.informatik.tu-darmstadt.de>
2021-02-22 10:28:20 +0100

    init

---
+++ runs/download_pretrained_models.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget http://public.ukp.informatik.tu-darmstadt.de/persona-consistent-dialogue/runs/Jun03_00-25-57_krusty.zip
+wget http://public.ukp.informatik.tu-darmstadt.de/persona-consistent-dialogue/runs/Nov09_21-11-05_krusty.zip
+wget http://public.ukp.informatik.tu-darmstadt.de/persona-consistent-dialogue/runs/Oct12_23-41-37_krusty.zip
